b21fe7d3a90938e5c0efab40cca219d624826d46
AnushkaaPatil605 <patilanushka605@gmail.com>
2026-04-23 18:13:49 +0530

    Refactor test cases and add first class and pass tests

--- devops project/test_app.py
+++ devops project/test_app.py
@@ -3,5 +3,11 @@ from app import calculate_grade
 def test_distinction():
     assert calculate_grade(80) == "Distinction"
 
+def test_first_class():
+    assert calculate_grade(65) == "First Class"
+
+def test_pass():
+    assert calculate_grade(50) == "Pass"
+
 def test_fail():
     assert calculate_grade(30) == "Fail"
